001dcd7729dbe7dc5040ce7df45bfb8e970b0001
GitHubOdix <azaheni@smartgraphe.com>
2019-08-27 19:36:59 +0100

    SecurityConfig : niveau basic

---
+++ src/main/java/com/cvtheque/org/service/UtilisateurService.java
@@ -0,0 +1,9 @@
+package com.cvtheque.org.service;
+
+import com.cvtheque.org.model.Utilisateur;
+
+public interface UtilisateurService {
+	
+	public void authenticate(Utilisateur utilisateur);
+
+}
---
+++ src/main/java/com/cvtheque/org/service/UtilisateurServiceaImpl.java
@@ -0,0 +1,11 @@
+package com.cvtheque.org.service;
+
+import com.cvtheque.org.model.Utilisateur;
+
+public class UtilisateurServiceaImpl implements UtilisateurService{
+	
+	public void authenticate(Utilisateur utilisateur) {
+		System.out.print("UtilisateurServiceaImpl");
+	}
+
+}
