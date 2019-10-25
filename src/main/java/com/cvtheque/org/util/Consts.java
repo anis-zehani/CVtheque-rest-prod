6c89b4b4d3809dcf884f7505c539f79ce3b6cda7
GitHubOdix <azaheni@smartgraphe.com>
2019-10-25 20:51:49 +0100

    Revue CSS Design Responsif

--- src/main/java/com/cvtheque/org/util/Consts.java
+++ src/main/java/com/cvtheque/org/util/Consts.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 
 public final class Consts {
 	
-	        // Mes messages de Notification
+	        // Tou mes messages de Notification
 			public static final String objetMsgNotificationCandidatAjoute = "Candidat ajouté";
 			public static final String corpsMsgNotificationCandidatAjouteLinkedin = "Création d'un nouveau compte candidat via Linkedin";
 			public static final String corpsMsgNotificationCandidatAjouteFormulaire = "Création d'un nouveau compte candidat via formulaire";
@@ -19,7 +19,7 @@ public final class Consts {
 			public static final String objetMsgNotificationModificationOpportunite = "Opportunité modifiée";
 			public static final String corpsMsgNotificationModificationOpportunite = "Une opportunité vient d'être modifiée par un partenaire";
 			
-/*
+
 			// Pour les urls dans les emails envoyés
 			public static final String urlPlatformeResetPassword = "http://plateforme.odix.fr/reset-password/";
 			public static final String urlPlatformeActivateCandidat = "http://plateforme.odix.fr/activation-compte/";
@@ -35,8 +35,8 @@ public final class Consts {
 			public static final Path rootLocationCvOriginal = Paths.get("/uploads");
 			//Emplacement fichier sur le serveur physique : container Rest
 			public static final Path rootLocationFichierRappel = Paths.get("/uploads");
-*/
 
+/*
 			// Pour les urls dans les emails envoyés
 			public static final String urlPlatformeResetPassword = "http://localhost:4200/reset-password/";
 			public static final String urlPlatformeActivateCandidat = "http://localhost:4200/activation-compte/";
@@ -52,5 +52,5 @@ public final class Consts {
 			public static final Path rootLocationCvOriginal = Paths.get("/var/tmp/uploads/");
 			//Emplacement fichier sur le serveur physique : container Rest
 			public static final Path rootLocationFichierRappel = Paths.get("/var/tmp/uploads/");	
-
+*/
 }
